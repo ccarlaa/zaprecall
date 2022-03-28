4ad13271e1fc87ee8fc486f1be036fc1e6d8c157
Luana Lessa <lessalsn@gmail.com>
2022-03-28 18:23:21 -0300

    chore: Code review

--- projeto8-zaprecall/src/Header.js
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <header className="header">
-            <img src="./images/logo.png" alt=""/>
-            <h1>ZapRecall</h1>
-        </header>
-    )
-}
